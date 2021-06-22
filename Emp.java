01b6d898e826e005b0dbfd75ac4ffd365b328fff
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 19:51:12 +0530

    Checking the employee is present or not

--- Emp.java
+++ Emp.java
@@ -3,7 +3,12 @@ package com.employeewage;
 public class Emp {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to the Employee Wage program");
+		int IS_FULL_TIME = 1;
+		double empcheck = Math.floor(Math.random()*10)%2;
+		if(empcheck == IS_FULL_TIME)
+		System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 
 	}
 
