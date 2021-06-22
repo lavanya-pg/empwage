0d849a3c21baebc92d11401430c7b0f65b9b57ba
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 19:59:59 +0530

    Assuming workinghours and wage

--- Emp.java
+++ Emp.java
@@ -3,9 +3,11 @@ package com.employeewage;
 public class Emp {
 
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
+		int FULLDAY = 8;
+		int WAGE_PERHOUR = 20;
+		double IS_EMP_PRESENT = 1.0;
 		double empcheck = Math.floor(Math.random()*10)%2;
-		if(empcheck == IS_FULL_TIME)
+		if(empcheck == IS_EMP_PRESENT)
 		System.out.println("Employee is present");
 		else
 			System.out.println("Employee is absent");
