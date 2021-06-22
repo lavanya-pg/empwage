f91a11af64fcb3c6af34b6eb8a344e99f0990d66
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 22:19:01 +0530

    Assuming parttime hours for employee

--- Emp.java
+++ Emp.java
@@ -3,15 +3,25 @@ package com.employeewage;
 public class Emp {
 
 	public static void main(String[] args) {
-		int FULLDAY = 8;
+		int IS_PARTTIME_PRESENT = 2;
+		int IS_FULLTIME_PRESENT = 1;
 		int WAGE_PERHOUR = 20;
-		double IS_EMP_PRESENT = 1.0;
-		double empcheck = Math.floor(Math.random()*10)%2;
-		if(empcheck == IS_EMP_PRESENT)
+		int FULLDAY_HOUR = 8;
+		int PARTTIME_HOUR = 4;
+		int dailywage = 0;
+		double empcheck = Math.floor(Math.random()*10)%3;
+		System.out.println(empcheck);
+		if(IS_FULLTIME_PRESENT == empcheck) {
 		System.out.println("Employee is present");
-		else
+		dailywage = WAGE_PERHOUR * FULLDAY_HOUR;
+		} 
+		else if(IS_PARTTIME_PRESENT == empcheck) {
+			System.out.println("Employee is present for partime");
+			dailywage = WAGE_PERHOUR * PARTTIME_HOUR;
+			}
+		else {
 			System.out.println("Employee is absent");
-
-	}
-
+		}
+			
+		}
 }
