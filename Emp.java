61b022bef10cdbe0d909470eb1e8fd7b88667afc
lavanya-pg <lavanyapg8@gmail.com>
2021-06-23 17:57:02 +0530

    calculating the wages still the working hours and working days reached

--- Emp.java
+++ Emp.java
@@ -1,42 +1,47 @@
 package com.employeewage;
 
 public class Emp {
-
-	public static void main(String[] args) {
-		int IS_PARTTIME_PRESENT = 2;
-		int IS_FULLTIME_PRESENT = 1;
-		int WAGE_PERHOUR = 20;
-		int FULLDAY_HOUR = 8;
-		int PARTTIME_HOUR = 4;
-		int NUM_OF_WORKINGDAYS = 20;
-		int dailywage = 0;
+		public static final int IS_PARTTIME_PRESENT = 2;
+		public static final int IS_FULLTIME_PRESENT = 1;
+		public static final int EMPLOYEE_WAGE_PERHOUR = 20;
+		public static final int MAX_HOURS_ALLOWED = 100;
+		public static final int DAYS_IN_A_MONTH = 20;
+		public static void main(String[] args) {
+			int monthlywage =0;
+			int totalworkingDays =0;
+			int totalEmployeeHours =0;
+			
+			while(totalEmployeeHours < MAX_HOURS_ALLOWED && totalworkingDays < DAYS_IN_A_MONTH) 
+			{
+				int dailyEmpwage =0;
+				totalworkingDays++;
+				int empHours = 0;
+				
 		double empcheck = Math.floor(Math.random()*10)%3;
-		System.out.println(empcheck);
-//		if(IS_FULLTIME_PRESENT == empcheck) {
-//		System.out.println("Employee is present");
-//		dailywage = WAGE_PERHOUR * FULLDAY_HOUR;
-//		} 
-//		else if(IS_PARTTIME_PRESENT == empcheck) {
-//			System.out.println("Employee is present for partime");
-//			dailywage = WAGE_PERHOUR * PARTTIME_HOUR;
-//			}
-//		else {
-//			System.out.println("Employee is absent");
-//		}
+
 		switch ((int)empcheck) {
-		case 1: 
-			System.out.println("Employee is present");
-			dailywage = WAGE_PERHOUR * FULLDAY_HOUR;
+		case IS_FULLTIME_PRESENT:
+			empHours = 8;
+			System.out.println("employee is present for full time");
 			break;
-		case 2 : 
-			System.out.println("Employee is present for partime");
-			dailywage = WAGE_PERHOUR * PARTTIME_HOUR;
+		case IS_PARTTIME_PRESENT: 
+			empHours = 4;
+			System.out.println("employee is present for part time");
 			break;
 
 		default:
+			empHours = 0;
 			System.out.println("Employee is absent");
+			break;
+			}
+			dailyEmpwage = EMPLOYEE_WAGE_PERHOUR * empHours;
+			totalEmployeeHours = totalEmployeeHours * empHours;
+			monthlywage = monthlywage +dailyEmpwage;
+			System.out.println("employee wage is: "+ dailyEmpwage);
+		}	
+			System.out.println("total employee hours : "+ totalEmployeeHours);
+			System.out.println("total employee working days : "+ totalworkingDays);
+			System.out.println("monthly employee wage : "+ monthlywage);
+	}
 			
-		}
-			System.out.println("employee wage is :" + dailywage);
-		}
 }
